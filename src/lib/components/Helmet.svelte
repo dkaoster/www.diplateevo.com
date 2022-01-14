5a6fb46ea4220fdf0a94dde2f20930a805b0bf3d
Daniel Kao <dkao@diplateevo.com>
2022-01-14 21:54:35 +0800

    Fix Meta Image

--- src/lib/components/Helmet.svelte
+++ src/lib/components/Helmet.svelte
@@ -24,12 +24,12 @@
   <meta property="og:title" content={metaTitle || title}>
   <meta property="og:type" content={type} />
   <meta property="og:url" content={url} />
-  <meta property="og:image" content={image} />
+  <meta property="og:image" content={`https://www.diplateevo.com${image}`} />
   <meta property="og:site_name" content={siteName || title} />
   <meta property="og:locale" content={locale} />
   {#if description}<meta property="og:description" content={description}>{/if}
 
-  <meta name="twitter:image" content={image}>
+  <meta name="twitter:image" content={`https://www.diplateevo.com${image}`}>
   <meta name="twitter:card" content="summary_large_image">
   <meta name="twitter:title" content={metaTitle || title} />
   <meta name="twitter:description" content={description}>
