309e0700b7dabc069573cb3a08b3f8a217fb7bf4
dkaoster <dkao@diplateevo.com>
2022-01-17 15:05:06 +0000

    Deploying to gh-pages from  @ 2e65e6512c26b7b0f912f2ac3333c5bb5e9d7153 🚀

---
+++ _app/chunks/image-15d1cba6.js
@@ -0,0 +1,1 @@
+const o=(e,s,n)=>{const t=e.split("."),a=t.slice(0,-1).join("."),i=t[t.length-1];return`${a}@${s}w.${n||i}`};export{o as s};
--- _app/chunks/image-32862358.js
+++
@@ -1,1 +0,0 @@
-const n=(t,e,i="jpg")=>`${t.split(".").slice(0,-1).join(".")}@${e}w.${i}`;export{n as s};
