79dd180e67627d61439359f1923f53d97fa09a77
dkaoster <dkao@diplateevo.com>
2022-08-08 06:05:28 +0000

    Deploying to gh-pages from  @ a517962b8a504df07599e5b9d607b72ece2c6347 🚀

---
+++ _app/immutable/chunks/index-7ef46866.js
@@ -0,0 +1,1 @@
+import{a5 as n}from"./index-6e742aee.js";function r(t,{delay:o=0,duration:e=400,easing:a=n}={}){const i=+getComputedStyle(t).opacity;return{delay:o,duration:e,easing:a,css:c=>`opacity: ${c*i}`}}export{r as f};
