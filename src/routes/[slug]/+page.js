7a1eda4c92066f5114b35f717e5998b160e8653f
Daniel Kao <dkao@diplateevo.com>
2024-12-06 02:14:00 +0800

    Fix RSS (#32)

--- src/routes/[slug]/+page.js
+++ src/routes/[slug]/+page.js
@@ -2,7 +2,7 @@ import { error } from '@sveltejs/kit';
 
 export async function load({ fetch, params: { slug } }) {
   // Get the posts and pagination
-  const data = await fetch(`/content/${slug}.json`).then((res) => res.json());
+  const data = await fetch(`/content/${slug}.json`).then((res) => (res.ok ? res.json() : {}));
 
   // Return the data for this slug
   if (Object.keys(data).length) return data;
